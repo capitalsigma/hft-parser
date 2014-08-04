e467153db0b25e1bf40f4d6d138fd409d1356cfa
pscollins@uchicago.edu <pscollins@uchicago.edu>
2014-08-04 09:21:48 -0500

    verifier passing unit tests for one group, testing multiple groups

--- src/main/java/com/hftparser/verifiers/DiffElement.java
+++ src/main/java/com/hftparser/verifiers/DiffElement.java
@@ -29,11 +29,67 @@ public class DiffElement {
                 '}';
     }
 
+    public String deepToString() {
+        return "DiffElement{" +
+                "expected=" + (expected != null ? expected.getPath(): null) +
+                ", actual=" + (actual != null ? actual.getPath() : null) +
+                ", index=" + index +
+                '}';
+    }
+
     public HDF5LinkInformation getExpected() {
         return expected;
     }
 
     public HDF5LinkInformation getActual() {
         return actual;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        DiffElement that = (DiffElement) o;
+
+        if (index != that.index) {
+            return false;
+        }
+//        we compare on paths
+//        if (actual != null ? !actual.equals(that.actual) : that.actual != null) {
+//            return false;
+//        }
+//        if (expected != null ? !expected.equals(that.expected) : that.expected != null) {
+//            return false;
+//        }
+
+        return compareLinkInformation(this.expected, that.expected) && compareLinkInformation(this.actual, that.actual);
+    }
+
+
+    private static boolean compareLinkInformation(HDF5LinkInformation expected, HDF5LinkInformation actual) {
+        if ((expected == null) && (actual == null)) {
+            return true;
+        } else if ((expected == null) || (actual == null)) {
+            return false;
+        } else {
+            return (expected.getType() == actual.getType()) && (expected.getPath().equals(actual.getPath()));
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int result = expected != null ? expected.hashCode() : 0;
+        result = 31 * result + (actual != null ? actual.hashCode() : 0);
+        result = 31 * result + index;
+        return result;
     }
 }
--- src/test/java/com/hftparser/verifiers/HDF5CompoundVerifierTest.java
+++ src/test/java/com/hftparser/verifiers/HDF5CompoundVerifierTest.java
@@ -3,7 +3,6 @@ package com.hftparser.verifiers;
 import ch.systemsx.cisd.hdf5.HDF5Factory;
 import ch.systemsx.cisd.hdf5.IHDF5Writer;
 import com.hftparser.readers.WritableDataPoint;
-import com.hftparser.writers.HDF5CompoundDSBridgeBuilder;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,15 +10,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class HDF5CompoundVerifierTest {
-    String TEST_DS_PATH = "src/test/resources/aapl-first-201.h5";
-    String TEST_DS_MISSING_LINE_PATH = "src/test/resources/aapl-first-200.h5";
+    String TEST_H5_PATH = "src/test/resources/aapl-first-201.h5";
+    String TEST_H5_MISSING_LINE_PATH = "src/test/resources/aapl-first-200.h5";
+    String TEST_DS_PATH = "/AAPL/books";
+
+    String TEST_H5_DIFFERENT_GROUPS = "src/test/resources/full-first-200.h5";
+
     HDF5CompoundVerifier<WritableDataPoint> verifier;
 
 
     @Before
     public void setUp() throws Exception {
-
-
     }
 
     @After
@@ -28,13 +29,13 @@ public class HDF5CompoundVerifierTest {
     }
 
     private void setVerifierForSame() throws Exception {
-        setVerifierForPaths(TEST_DS_PATH, TEST_DS_PATH);
+        setVerifierForPaths(TEST_H5_PATH, TEST_H5_PATH);
 
     }
 
-    private void setVerifierForPaths(String actualPath, String expectedPath) throws Exception {
-        IHDF5Writer expectedWriter = HDF5Factory.open(TEST_DS_PATH);
-        IHDF5Writer actualWriter = HDF5Factory.open(TEST_DS_PATH);
+    private void setVerifierForPaths(String expectedPath, String actualPath) throws Exception {
+        IHDF5Writer expectedWriter = HDF5Factory.open(expectedPath);
+        IHDF5Writer actualWriter = HDF5Factory.open(actualPath);
 
         verifier = new HDF5CompoundVerifier<>(actualWriter, expectedWriter, WritableDataPoint.class);
 
@@ -63,7 +64,16 @@ public class HDF5CompoundVerifierTest {
 
         assertNotNull("Should have found a difference", diff);
 
-        System.out.println("Diff: " + diff.toString());
+        System.out.println("Diff: " + diff.deepToString());
+
+        assertNotNull(diff.getExpected());
+        assertNotNull(diff.getActual());
+
+        assertEquals(TEST_DS_PATH, diff.getExpected().getPath());
+        assertEquals(TEST_DS_PATH, diff.getActual().getPath());
+
+        assertEquals(diff.getIndex(), 200);
+
 
         //         TODO: figure out how to compare these
         return diff;
@@ -71,17 +81,19 @@ public class HDF5CompoundVerifierTest {
 
     @Test
     public void testDiffDsMissingLeft() throws Exception {
-        DiffElement diff = testDiffDSMissing(TEST_DS_PATH, TEST_DS_MISSING_LINE_PATH);
-
-        assertNull(diff.getExpected());
-        assertNotNull(diff.getActual());
+        DiffElement diff = testDiffDSMissing(TEST_H5_PATH, TEST_H5_MISSING_LINE_PATH);
     }
 
     @Test
     public void testDiffDsMissingRight() throws Exception {
-        DiffElement diff = testDiffDSMissing(TEST_DS_MISSING_LINE_PATH, TEST_DS_PATH);
+        DiffElement diff = testDiffDSMissing(TEST_H5_MISSING_LINE_PATH, TEST_H5_PATH);
+    }
 
-        assertNotNull(diff.getExpected());
-        assertNull(diff.getActual());
+    @Test
+    public void testDiffDsMissingEqual() throws Exception {
+        DiffElement left = testDiffDSMissing(TEST_H5_PATH, TEST_H5_MISSING_LINE_PATH);
+        DiffElement right = testDiffDSMissing(TEST_H5_MISSING_LINE_PATH, TEST_H5_PATH);
+
+        assertEquals(left, right);
     }
 }
